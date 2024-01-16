e5e15372267cd7a4f7cae1cb4299563847323aaa
fbelkhir <155823344+fbelkhir@users.noreply.github.com>
2024-01-16 20:39:36 +0100

    Update point.h

--- point.h
+++ point.h
@@ -5,6 +5,17 @@ struct Point {
     float x,y,z;
     Point(float a=0, float b=0, float c=0) {set(a,b,c);}
     void set(float a, float b, float c) {x=a;y=b;z=c;}
+    float getX() const {
+        return x;
+    }
+
+    float getY() const {
+        return y;
+    }
+
+    float getZ() const {
+        return z;
+    }
 };
 
 #endif
